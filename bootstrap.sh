2fa88dcc96415edb6a089f32a67864a035259558
Steve Strassmann <steve@strassmann.com>
2016-07-11 22:38:08 -0400

    remove stale comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,6 @@ ln -s /vagrant /home/vagrant/sync
 yum -y update
 
 # Install basic stuff
-#sudo rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
-
 yum -y install curl git unzip ntp emacs-nox libcurl-devel binutils gcc-c++ epel-release
 yum -y install gdal-devel gdal geos 
 yum -y install python-devel gcc postgresql94-server postgresql94-devel postgresql-contrib 
@@ -31,22 +29,4 @@ pip install django-rest-swagger
 pip install markdown
 pip install psycopg2
 
-# install GDAL
-#cd /tmp
-#curl -O http://download.osgeo.org/gdal/2.1.0/gdal-2.1.0.tar.gz
-#tar xzf gdal-2.1.0.tar.gz 
-#cd gdal-2.1.0/
-#./configure
-#make
-#make install
-
-# install Geos
-# see https://docs.djangoproject.com/en/1.9/ref/contrib/gis/install/geolibs/
-#cd /tmp
-#curl -O http://download.osgeo.org/geos/geos-3.5.0.tar.bz2
-#tar xjf geos-3.5.0.tar.bz2
-#cd geos-3.5.0
-#./configure
-#make
-#make install
 

d0b4f0357681c7aa8afdceadecae4cb81da606d9
Steve Strassmann <steve@strassmann.com>
2017-01-23 17:28:42 -0500

    change load order for migrations

--- django/microns/settings.py
+++ django/microns/settings.py
@@ -173,6 +173,8 @@ API_VERSION = 'v1'
 
 THEBOSS_CONFIG = os.path.join(BASE_DIR, 'theboss.cfg')
 
+LOGOUT_URL = 'https://api.theboss.io/openid/logout'
+
 auth_uri = "https://auth.theboss.io/auth/realms/BOSS"
 client_id = "church" # Client ID configured in the Auth Server
 
@@ -187,6 +189,3 @@ else:
 from bossoidc.settings import *
 configure_oidc(auth_uri, client_id, public_uri)
 
-print('**** ENV ****', os.environ)
-
-print('**** DATABASES ****', DATABASES)

88699bd2eededb439728060dabddcf3fc792f1d8
Steve Strassmann <steve@strassmann.com>
2017-03-13 11:46:54 -0400

    cleanup: remove obsolete test, obsolete ndio module

--- django/microns/tests/test_views.py
+++ django/microns/tests/test_views.py
@@ -3,6 +3,5 @@ from django.test import SimpleTestCase
 class HomePageViewTestCase(SimpleTestCase):
     def test_request_home_page(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 301)
-        self.assertEqual(response.url, 'https://wyssmicrons.github.io/MIDAS-MICrONS/')
+        self.assertEqual(response.status_code, 200)
 
